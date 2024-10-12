f7311ac7f3151bda0fb7bfecaa5596487314a727
ArsenyIT <st.2.udalov@gmail.com>
2024-10-12 18:05:08 +0300

    д/з 12.10.2024

---
+++ hb/main_3.py
@@ -0,0 +1,12 @@
+from hitbox import Hitbox
+
+hb1 = Hitbox(x = 0,y = 100, width = 100, height = 100)
+hb2 = Hitbox(x = 75,y = 100, width = 100, height = 100)
+hb3 = Hitbox(x = 200,y = 100, width = 100, height = 100)
+
+intersection = hb1.intersects(hb2)
+print(intersection)
+intersection2 = hb2.intersects(hb3)
+print(intersection2)
+intersection3 = hb3.intersects(hb1)
+print(intersection3)
